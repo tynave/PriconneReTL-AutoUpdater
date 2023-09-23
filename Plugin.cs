f1ae70506ddffe4bd2e855d4a271ebad22d275c9
Farleena <tynave@gmail.com>
2023-09-24 00:32:49 +0200

    Implemented version checks into dll. Replaced GetGamePath method with Bepinex.Paths.GameRootPath

--- Plugin.cs
+++ Plugin.cs
@@ -6,25 +6,56 @@ using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace PriconneReTLAutoUpdater;
 
-[PatcherPluginInfo("PriconneReTlAutoUpdater", "PriconneReTLAutoUpdater by Farleena", "1.0.0")]
+[PatcherPluginInfo("PriconneReTLAutoUpdater", "PriconneReTLAutoUpdater by Farleena", "1.0.0")]
 class EntrypointPatcher : BasePatcher
 {
+    private static string githubAPI = "https://api.github.com/repos/ImaterialC/PriconneRe-TL";
+    private string assetLink;
     private string priconnePath;
-    private bool priconnePathValid;
+    private string localVersion;
+    private bool localVersionValid;
+    private string latestVersion;
+    private bool latestVersionValid;
 
+    internal static new ManualLogSource Logger;
     public override void Initialize() 
     {
-        var myLogSource = BepInEx.Logging.Logger.CreateLogSource("PriconneReTLAutoUpdater");
+        EntrypointPatcher.Logger = base.Log;
+
         try
         {
-            /*string exePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "PriconneReTLAutoUpdaterApp.exe");
+            priconnePath = Paths.GameRootPath;
+            (localVersion, localVersionValid) = GetLocalVersion();
+            (latestVersion, latestVersionValid, assetLink) = GetLatestRelease();
+
+            if (!latestVersionValid || !localVersionValid)
+            {
+                Logger.LogError("Could not determine local version or latest release! Cannot continue!");
+                return;
+            }
+
+            Logger.LogInfo($"Arguments: {priconnePath}, {localVersion}, {latestVersion} {assetLink}");
+
+            int versioncompare = localVersion.CompareTo(latestVersion);
+
+            if (versioncompare == 0)
+            {
+                Logger.LogWarning("You already have the latest version installed! Skipping update!");
+                return;
+            }
+
+            string exePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "PriconneReTLAutoUpdaterApp.exe");
 
             ProcessStartInfo startInfo = new ProcessStartInfo
             {
                 FileName = exePath,
+                Arguments = $"{priconnePath} {localVersion} {latestVersion} {assetLink}"
             };
 
             Process process = new Process
@@ -32,63 +63,85 @@ class EntrypointPatcher : BasePatcher
                 StartInfo = startInfo
             };
 
+            Logger.LogInfo("New version found! Starting PriconneReTLAutoUpdaterApp..");
+
             process.Start();
-
-            process.WaitForExit();*/
-
-            (priconnePath, priconnePathValid) = GetGamePath();
+            process.WaitForExit();
 
         }
         catch (Exception ex)
         {
-            myLogSource.LogError("An error occurred: " + ex.Message);
+            Logger.LogError("An error occurred: " + ex.Message);
         }
 
 
     }
     public override void Finalizer() { }
 
-    public (string priconnePath, bool priconnePathValid) GetGamePath()
+    public (string localVersion, bool localVersionValid) GetLocalVersion()
     {
-        var myLogSource = BepInEx.Logging.Logger.CreateLogSource("PriconneReTLAutoUpdater");
         try
         {
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            string versionString = assembly.GetName().Version.ToString();
+            string versionFilePath = Path.Combine(priconnePath, "BepInEx", "Translation", "en", "Text", "Version.txt");
 
-            myLogSource.LogInfo($"PriconneReTLAutoUpdater version: {versionString}");
-
-            string cfgFileContent = File.ReadAllText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "dmmgameplayer5", "dmmgame.cnf"));
-            dynamic cfgJson = JsonConvert.DeserializeObject(cfgFileContent);
-
-            if (cfgJson != null && cfgJson.contents != null)
+            if (!File.Exists(versionFilePath))
             {
-                foreach (var content in cfgJson.contents)
-                {
-                    if (content.productId == "priconner")
-                    {
-                        priconnePath = content.detail.path;
-                        // priconnePath = "C:\\Test"; // -- set fixed path for testing purposes
-                        myLogSource.LogInfo("Found Princess Connect Re:Dive in " + priconnePath);
-                        return (priconnePath, priconnePathValid = true);
-                    }
-                }
+                Logger.LogError("Version file not found!");
+                return (localVersion: null, localVersionValid = false);
             }
-            myLogSource.LogError("Cannot find the game path! Did you install Princess Connect Re:Dive from DMMGamePlayer?");
-            
-            return (priconnePath = "Not found", priconnePathValid = false);
-        }
-        catch (FileNotFoundException)
-        {
-            myLogSource.LogError("Cannot find the DMMGamePlayer config file! Do you have DMMGamePlayer installed?");
-            
-            return (priconnePath = "Not found", priconnePathValid = false);
+            string rawVersionFile = File.ReadAllText(versionFilePath);
+            localVersion = System.Text.RegularExpressions.Regex.Match(rawVersionFile, @"\d{8}[a-z]?").Value;
+
+            if (localVersion == "")
+            {
+                Logger.LogError("Version string does not match regex pattern!");
+                return (localVersion: null, localVersionValid: false);
+            }
+
+            Logger.LogInfo($"Local Version: {localVersion}");
+            return (localVersion, localVersionValid: true);
+
         }
         catch (Exception ex)
         {
-            myLogSource.LogError("Error getting game path: " + ex.Message);
-            
-            return (priconnePath = "ERROR!", priconnePathValid = false);
+            Logger.LogError("Error getting local version: " + ex.Message);
+            return (localVersion: null, localVersionValid: false);
+        }
+    }
+
+    static (string latestVersion, bool latestVersionValid, string assetLink) GetLatestRelease()
+    {
+        try
+        {
+            string releaseUrl = githubAPI + "/releases/latest";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.Timeout = TimeSpan.FromSeconds(10);
+                client.DefaultRequestHeaders.Add("User-Agent", "PriconneTLUpdater");
+
+                HttpResponseMessage response = client.GetAsync(releaseUrl).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = response.Content.ReadAsStringAsync().Result;
+                    dynamic releaseJson = JsonConvert.DeserializeObject(responseBody);
+                    string version = releaseJson.tag_name;
+                    string assetLink = releaseJson.assets[0].browser_download_url;
+                    Logger.LogInfo($"Latest Release Version: {version}");
+                    return (latestVersion: version, latestVersionValid: true, assetLink: assetLink);
+                }
+                else
+                {
+                    Logger.LogError($"Error getting latest release: {response.ReasonPhrase}");
+                    return (latestVersion: null, latestVersionValid: false, assetLink: null);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError("Error getting latest release: " + ex.Message);
+            return (latestVersion: null, latestVersionValid: false, assetLink: null);
         }
     }
 
